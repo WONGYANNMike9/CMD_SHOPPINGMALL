d02ea7755d6d63307923b147d387ecd8374fff20
WangYan <wyzy19992017@gmail.com>
2022-03-28 10:33:13 +0800

    Public setting

--- mall.py
+++ mall.py
@@ -11,8 +11,7 @@ class GUEST_OP(object):
     """顾客购物类"""
     def __init__(self):
         """连接数据库"""
-        self.conn = connect(host='49.235.89.99',port=3306,user='remoteu1',password='190450',
-            database='7640TEST')
+        self.conn = connect(#链接字符串复制到这里)
         self.cursor = self.conn.cursor()
 
     def __del__(self):
